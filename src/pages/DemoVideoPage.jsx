41befe5761ca0f92ffd8920ceb95e65e091a8b07
bobbygour30 <bobby30gour@gmail.com>
2025-09-30 18:25:05 +0530

    updated

--- src/pages/DemoVideoPage.jsx
+++ src/pages/DemoVideoPage.jsx
@@ -45,7 +45,7 @@ const DemoVideoPage = () => {
           <iframe
             width="100%"
             height="100%"
-            src="https://www.youtube.com/embed/dQw4w9WgXcQ" // Replace with real astrology/educational video
+            src="https://www.youtube.com/watch?v=RKpXxsAEJJI&t=1s" // Replace with real astrology/educational video
             title="Demo Video"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
