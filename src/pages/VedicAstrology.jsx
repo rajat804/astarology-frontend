a05ca2bd2736d96997380f29f91746692638eb48
Rajat <rajat804@gmail.com>
2026-06-04 15:04:54 +0530

    vedic astrology udpate

--- src/pages/VedicAstrology.jsx
+++ src/pages/VedicAstrology.jsx
@@ -27,6 +27,7 @@ import {
 import { FaInfinity, FaMoon, FaStar, FaQuoteLeft, FaChartLine, FaGlobe,FaGlobeAsia  } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import CTA from "../components/common/CTA";
+import assets from "../assets/assets";
 
 const VedicAstrology = () => {
   const [isMobile, setIsMobile] = useState(false);
@@ -80,7 +81,7 @@ const VedicAstrology = () => {
         <div 
           className="absolute inset-0 bg-cover bg-center bg-no-repeat"
           style={{
-            backgroundImage: `url('https://nakshatraganak.com/img/curosel5.webp')`,
+            backgroundImage: `url(${assets.curosel5})`,
           }}
         >
           <div className="absolute inset-0 bg-gradient-to-b from-black/60 via-black/50 to-black/70" />
@@ -256,7 +257,7 @@ const VedicAstrology = () => {
             >
               <div className="relative rounded-2xl md:rounded-3xl overflow-hidden shadow-2xl">
                 <img
-                  src="https://nakshatraganak.com/img/service-2.jpg"
+                  src={assets.service2}
                   alt="Vedic Astrology Birth Chart"
                   className="w-full h-auto object-cover"
                   loading="lazy"
