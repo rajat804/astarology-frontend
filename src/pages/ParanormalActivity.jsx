4123b28d3c13b8bd6c966a6dba6567ae882ea51b
Rajat <rajat804@gmail.com>
2026-06-04 15:04:27 +0530

    paranormal acivity update

--- src/pages/ParanormalActivity.jsx
+++ src/pages/ParanormalActivity.jsx
@@ -22,6 +22,7 @@ import {
 import { FaPrayingHands, FaFeatherAlt, FaOm } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import CTA from "../components/common/CTA";
+import assets from "../assets/assets";
 
 const ParanormalActivity = () => {
   const [isMobile, setIsMobile] = useState(false);
@@ -75,7 +76,7 @@ const ParanormalActivity = () => {
         <div 
           className="absolute inset-0 bg-cover bg-center bg-no-repeat"
           style={{
-            backgroundImage: `url('https://nakshatraganak.com/img/curosel5.webp')`,
+            backgroundImage: `url(${assets.curosel5})`,
           }}
         >
           <div className="absolute inset-0 bg-gradient-to-b from-black/60 via-black/50 to-black/70" />
@@ -184,7 +185,7 @@ const ParanormalActivity = () => {
             >
               <div className="relative rounded-2xl md:rounded-3xl overflow-hidden shadow-2xl">
                 <img
-                  src="https://nakshatraganak.com/img/paranormal.jpg"
+                  src={assets.paranormal}
                   alt="Paranormal Activity Cleansing"
                   className="w-full h-auto object-cover"
                   loading="lazy"
